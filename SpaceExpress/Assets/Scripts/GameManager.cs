14bc01ca25784471e89708ea7714d8eee67dc6c5
RonCohen47 <138518157+RonCohen47@users.noreply.github.com>
2024-07-02 23:57:53 +0300

    Can place beams on click

--- SpaceExpress/Assets/Scripts/GameManager.cs
+++ SpaceExpress/Assets/Scripts/GameManager.cs
@@ -5,21 +5,42 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     [SerializeField] TrainManager _trainManager;
+    [SerializeField] GameObject _beam;
+    private bool beamMode = false;
 
+    public bool BeamMode { get => beamMode; set => beamMode = value; }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            PointnClick();
+        }
+    }
     public void PointnClick()
     {
-
+        if (BeamMode)
+        {
+            Debug.Log("Should Work");
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
         if (Physics.Raycast(ray, out hit))
         {
             Debug.Log("Hit: " + hit.point);
-            GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
-            cube.transform.position = hit.point;
+             GameObject newBeam = Instantiate(_beam);
+           newBeam.transform.position = hit.point;
         }
-
+        }
     }
 
+    public void EnableBeam()
+    {
+        if (BeamMode)
+        {
+            BeamMode = false;
+        }
+        else { BeamMode = true; }
+    }
 
     public void StartDrive()
     {
