3e40acbec94c530ed726e4ac2fd484bedd6410a7
RonCohen47 <138518157+RonCohen47@users.noreply.github.com>
2024-08-15 15:56:09 +0300

    Working Rails

--- SpaceExpress/Assets/Scripts/MagneticFieldScript.cs
+++ SpaceExpress/Assets/Scripts/MagneticFieldScript.cs
@@ -7,10 +7,20 @@ public class MagneticFieldScript : MonoBehaviour
     public float attractionForce = 10f;
     public float railWidth = 0.5f;
 
+    private void Awake()
+    {
+
+       // trainRb = GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody>();
+
+    }
     private void OnTriggerStay(Collider other)
     {
+        Debug.Log(trainRb.linearVelocity);
+
+
         if (other.gameObject.CompareTag(("Wheels")))
         {
+
             if (trainRb != null)
             {
                 Vector3 directionToRail = rail.position - other.transform.position;
@@ -18,6 +28,8 @@ public class MagneticFieldScript : MonoBehaviour
 
                 if (directionToRail.magnitude < railWidth)
                 {
+                    Debug.Log("Im here 1");
+
                     trainRb.position = new Vector3(rail.position.x, trainRb.position.y, rail.position.z);
                     trainRb.linearVelocity = Vector3.zero;
 
@@ -27,13 +39,16 @@ public class MagneticFieldScript : MonoBehaviour
                 }
                 else
                 {
+                    Debug.Log("Im here 2");
                     Quaternion targetRotation = Quaternion.Euler(rail.eulerAngles.x * -1,
                         trainRb.rotation.eulerAngles.y, trainRb.rotation.eulerAngles.z);
                     trainRb.rotation = Quaternion.Slerp(trainRb.rotation, targetRotation, Time.deltaTime * 5f);
-
-                    trainRb.AddForce(directionToRail.normalized * attractionForce);
+                    directionToRail.x = 140;
+                     trainRb.AddForce(directionToRail.normalized * attractionForce);
                 }
             }
+            
         }
+       
     }
 }
